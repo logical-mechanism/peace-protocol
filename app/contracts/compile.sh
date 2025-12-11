2992b3138d251499786ea253580986bb2f66c8ff
logicalmechanism <logicalmechanism@protonmail.com>
2025-12-11 01:07:09 -0800

    creating a setup file

--- app/contracts/compile.sh
+++ app/contracts/compile.sh
@@ -1,62 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-if command -v jq &> /dev/null; then
-    echo -e "\033[1;35m jq is installed and available on the PATH. \033[0m"
-else
-    echo -e "\033[1;31m jq is not installed or not available on the PATH. \033[0m"
-    echo -e "\033[1;33m sudo apt install -y jq \033[0m"
-    exit 1;
-fi
-
-if command -v python3 &> /dev/null; then
-    echo -e "\033[1;35m python3 is installed and available on the PATH. \033[0m"
-else
-    echo -e "\033[1;31m python3 is not installed or not available on the PATH. \033[0m"
-    echo -e "\033[1;33m sudo apt install -y python3 \033[0m"
-    exit 1;
-fi
-
-if python3 -c "import cbor2" 2>/dev/null; then
-    echo -e "\033[1;35m cbor2 is installed and available for python3. \033[0m"
-else
-    echo -e "\033[1;31m cbor2 is not installed or not available for python3. \033[0m"
-    echo -e "\033[1;33m sudo apt-get install python3-cbor2 \033[0m"
-    exit 1;
-fi
-
-if command -v sponge &> /dev/null; then
-    echo -e "\033[1;35m sponge is installed and available on the PATH. \033[0m"
-else
-    echo -e "\033[1;31m sponge is not installed or not available on the PATH. \033[0m"
-    echo -e "\033[1;33m sudo apt-get install more-utils \033[0m"
-    exit 1;
-fi
-
-if command -v aiken &> /dev/null; then
-    echo -e "\033[1;35m aiken is installed and available on the PATH. \033[0m"
-else
-    echo -e "\033[1;31m aiken is not installed or not available on the PATH. \033[0m"
-    echo -e "\033[1;33m https://github.com/aiken-lang/aiken \033[0m"
-    exit 1;
-fi
-
-if command -v cardano-cli &> /dev/null; then
-    echo -e "\033[1;35m cardano-cli is installed and available on the PATH. \033[0m"
-else
-    echo -e "\033[1;31m cardano-cli is not installed or not available on the PATH. \033[0m"
-    echo -e "\033[1;33m https://github.com/IntersectMBO/cardano-cli \033[0m"
-    exit 1;
-fi
-
-if command -v sha256sum &> /dev/null; then
-    echo -e "\033[1;35m sha256sum is installed and available on the PATH. \033[0m"
-else
-    echo -e "\033[1;31m sha256sum is not installed or not available on the PATH. \033[0m"
-    echo -e "\033[1;33m sudo apt install coreutils \033[0m"
-    exit 1;
-fi
-
 # create directories if dont exist
 mkdir -p contracts
 mkdir -p hashes
@@ -66,18 +10,8 @@ rm contracts/* || true
 rm hashes/* || true
 rm -fr build/ || true
 
-###############################################################################
-
-# remove all traces for production
-# aiken build --trace-level silent --trace-filter user-defined
-
-# keep all traces for development
-aiken build --trace-level verbose --trace-filter all
-
-###############################################################################
-
-genesis_tx_id=$(jq -r '.genesis_tx_id' config.json)
-genesis_tx_idx=$(jq -r '.genesis_tx_idx' config.json)
+genesis_tx_id=$(jq -r '.genesis_tx_id' ../config.json)
+genesis_tx_idx=$(jq -r '.genesis_tx_idx' ../config.json)
 genesis_tx_id_cbor=$(python3 -c "import cbor2;encoded=cbor2.dumps(bytes.fromhex('${genesis_tx_id}'));print(encoded.hex())")
 genesis_tx_idx_cbor=$(python3 -c "import cbor2;encoded=cbor2.dumps(${genesis_tx_idx});print(encoded.hex())")
 echo -e "\033[1;33m\nGenesis UTxO: ${genesis_tx_id}#${genesis_tx_idx} \033[0m"
