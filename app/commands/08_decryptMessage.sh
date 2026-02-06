658c45fdd5b503644f1f5a36b55e52e9c22b641f
logicalmechanism <logicalmechanism@protonmail.com>
2026-02-06 03:00:04 -0800

    scripts can fix on preprod, test e2e

--- app/commands/08_decryptMessage.sh
+++ app/commands/08_decryptMessage.sh
@@ -18,6 +18,13 @@ fi
 encryption_pid=$(cat ../contracts/hashes/encryption.hash)
 encryption_tkn=$(cat ../data/encryption.token)
 
+# stake key
+staking_credential=$(jq -r '.staking_credential' ../config.json)
+
+# encryption
+encryption_script_path="../contracts/contracts/encryption_contract.plutus"
+encryption_script_address=$(${cli} conway address build --payment-script-file ${encryption_script_path} --stake-key-hash ${staking_credential} ${network})
+
 asset=${encryption_pid}${encryption_tkn}
 
 tx_hashes=$(curl -X GET "https://preprod.koios.rest/api/v1/asset_txs?_asset_policy=${encryption_pid}&_asset_name=${encryption_tkn}&_history=true" -H 'accept: application/json' | jq -r '[.[].tx_hash][:-1]')
@@ -26,8 +33,8 @@ encryption_levels=$(curl -X POST "https://preprod.koios.rest/api/v1/tx_info" \
  -H 'accept: application/json' \
  -H 'content-type: application/json' \
  -d '{"_tx_hashes":'"${tx_hashes}"',"_inputs":false,"_metadata":false,"_assets":false,"_withdrawals":false,"_certs":false,"_scripts":true,"_bytecode":false}' | jq 'sort_by(.block_height) | reverse |
-  [.[0] | .outputs[] | select(.payment_addr.bech32 == "addr_test1zqrx3fkkfurpwphwaqe9ne27tn9qpkkn5p6rajs636pfntyxca55rx42vu7fv0dqfe94htjy34ysut82eypvhqhymfmqhet0m3") | .inline_datum.value.fields[3,4]] +
-  [.[1:][] | .outputs[] | select(.payment_addr.bech32 == "addr_test1zqrx3fkkfurpwphwaqe9ne27tn9qpkkn5p6rajs636pfntyxca55rx42vu7fv0dqfe94htjy34ysut82eypvhqhymfmqhet0m3") | .inline_datum.value.fields[4]]
+  [.[0] | .outputs[] | select(.payment_addr.bech32 == "'${encryption_script_address}'") | .inline_datum.value.fields[3,4]] +
+  [.[1:][] | .outputs[] | select(.payment_addr.bech32 == "'${encryption_script_address}'") | .inline_datum.value.fields[4]]
 ')
 
 
